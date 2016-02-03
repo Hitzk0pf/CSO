987b7090b4e2ad00552299d111ce717ec363ea5f
Hitzk0pf <sebipuck@hotmail.com>
2016-02-03 11:39:24 +0100

    Vorbereiten des Templates für die eigentlichen Seiten der Website

--- js/js.js
+++ js/js.js
@@ -4,6 +4,7 @@ $(function () {
     var active = false;
     $("body").on("click", ".mobile-navi-button", function (event) {
         $('#header #navigation .content nav.element .menu').slideToggle(150);
+        $('#navigation .language-content').fadeOut(20);
     });
 
     $(window).resize(function () {
@@ -15,5 +16,11 @@ $(function () {
 
       cacheScreenWidth = $(window).width();
 
+    });
+});
+
+$(function () {
+    $("body").on("click", "#navigation .locale", function (event) {
+      $('#navigation .language-content').fadeToggle(20);
     });
 });
