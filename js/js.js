ed368fe07751ca129e513f6f16cd0bbbb47fadd0
Hitzk0pf <sebipuck@hotmail.com>
2016-02-17 12:26:13 +0100

    added sticky header

--- js/js.js
+++ js/js.js
@@ -24,3 +24,111 @@ $(function () {
       $('#navigation .language-content').fadeToggle(20);
     });
 });
+
+//STICKY NAVI
+var stickyNaviBuffer = 150;
+var stickyNaviActive = false;
+var stickyNaviBreakPoint = $("#header #banner").height() + stickyNaviBuffer;
+
+$(function() {
+    $(window).on('scroll', function() {
+
+      if(window.pageYOffset > stickyNaviBreakPoint) {
+
+        //enable sticky navi
+        if(stickyNaviActive == false) {
+
+          stickyNaviService_enable();
+
+        }
+
+      } else {
+
+        //disable sticky navi
+        if(stickyNaviActive == true) {
+
+          stickyNaviService_disable();
+
+        }
+
+      }
+
+    });
+});
+
+function stickyNaviService_enable() {
+
+  var navigation_sticky = $("#header .navigation-sticky");
+  var navigation_border = $("#header .navigation-border");
+
+  navigation_sticky.css("top", "-100px");
+
+  navigation_sticky.css("position", "fixed");
+
+  navigation_border.fadeIn(20);
+
+  //show navigation
+  navigation_sticky.animate({
+
+    top: "0"
+
+  }, 300, function() {
+    // Animation complete.
+  });
+
+  /*
+  //add border
+  navigation_border.animate({
+
+    width: "100%"
+
+  }, 1, function() {
+    // Animation complete.
+  });
+  */
+
+  stickyNaviActive = true;
+
+}
+
+function stickyNaviService_disable() {
+
+  var navigation_sticky = $("#header .navigation-sticky");
+  var navigation_border = $("#header .navigation-border");
+
+  navigation_border.fadeOut(100);
+
+  navigation_sticky.hide();
+  navigation_sticky.css("position", "relative");
+  navigation_sticky.css("top", "0");
+  navigation_sticky.slideToggle(350);
+
+  /*
+  //hide navigation
+  navigation_sticky.animate({
+
+    top: "-100px"
+
+  }, 400, function() {
+    // Animation complete.
+  });
+  */
+
+  /*
+  //remove border
+  navigation_border.animate({
+
+    width: "0%"
+
+  }, 1, function() {
+    navigation_sticky.hide();
+    navigation_sticky.css("position", "relative");
+    navigation_sticky.css("top", "0");
+    navigation_sticky.slideToggle(200);
+  });
+  */
+
+  stickyNaviActive = false;
+
+
+}
